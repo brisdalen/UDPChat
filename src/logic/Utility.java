e0a5188ee0c527dec3f95f1ece5845e681a0d3cc
Bjørnar <bwendelborgrisdalen@gmail.com>
2020-01-12 23:32:41 +0100

    Created method in Utility for
    future handling of the ack BitSet

--- src/logic/Utility.java
+++ src/logic/Utility.java
@@ -3,6 +3,7 @@ package logic;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.util.BitSet;
 
 public class Utility {
 
@@ -69,5 +70,48 @@ public class Utility {
 
     public static DatagramPacket createPacket(byte[] message, InetAddress ip, int port) {
         return new DatagramPacket(message, message.length, ip, port);
+    }
+
+    /**
+     * updates a given BitSet in regards to how many packets are missed between the
+     * remote ack and the new ack
+     * @param original the BitSet to calculate on
+     * @param remoteAck the currently stored remote ack
+     * @param newAck the new ack to calculate from
+     */
+    public static void updateAcks(BitSet original, int remoteAck, int newAck) {
+        // if missedPackets is higher than 1, then we have some packet loss
+        int missedPackets = newAck - remoteAck;
+        // oldAcks is used for calculating the new BitSet of acknowledged packets
+        int oldAcks = 32 - missedPackets;
+
+        // Copy the bits that will remain after the shift
+        BitSet copy = new BitSet(32);
+        if(!(missedPackets >= 33)) {
+            copy = original.get(missedPackets, 33);
+        }
+
+        // Move the copied bits to the start of the BitSet
+        for(int i = 0; i < oldAcks; i++) {
+            original.set(i, copy.get(i));
+        }
+
+        // No need to set any bits to 0 if there are no missed packets
+        if(oldAcks <= 31 && oldAcks > 0) {
+            // otherwise set the bits between to 0
+            for (int i = oldAcks; i < 31; i++) {
+                original.set(i, false);
+            }
+        } else {
+            // Set all bits to 0 if all packets have been missed
+            for(int i = 0; i < 32; i++) {
+                original.set(i, false);
+            }
+        }
+
+        // Set the last (current) packet to true
+        original.set(31, true);
+
+        System.out.println("\n" + original);
     }
 }
